ce265a20ff1c3c1c88a695496b7dbe94316b41dd
Wes Hediger <wes.hediger@gmail.com>
2016-11-11 13:56:18 -0700

    add getcatagorysales()

--- data/squareup.js
+++ data/squareup.js
@@ -6,23 +6,27 @@ module.exports = {
   getSales: function(){
     return new Promise(function(fulfill, reject){
       var totalSales = 0;
+      var catagorySales = {};
       var options = { method: 'GET',
-                         url: 'https://connect.squareup.com/v1/996NZP3EEMBXN/payments?begin_time=2015-01-17T00:00:00Z&end_time=2015-01-18T00:00:00Z',
+                         url: 'https://connect.squareup.com/v1/996NZP3EEMBXN/payments?begin_time=2016-02-01T00:00:00Z&end_time=2016-02-02T00:00:00Z',
                     headers:
                          { 'postman-token': '5ceddfb1-9d9e-2b6e-e8a1-07200fc04676',
                            'cache-control': 'no-cache',
                             'content-type': 'application/json',
                                     accept: 'application/json',
+                                     limit: '200',
                              authorization: 'Bearer ' + process.env.STELLAS_ACCESS_TOKEN
                          }
                     }
       request(options, function(err, res, data){
         data = JSON.parse(data);
         totalSales = module.exports.getTotalSales(data);
+        catagorySales.catagories = module.exports.getCatagorySales(data);
+        console.log(catagorySales.catagories);
         if (err) reject(err)
         else {
           if (res.headers.link){
-            totalSales = module.exports.getMorePages(res.headers.link, totalSales)
+            totalSales = module.exports.getMorePages(res.headers.link, totalSales, data, catagorySales);
             fulfill(totalSales);
           }
           else fulfill(totalSales)
@@ -54,9 +58,12 @@ module.exports = {
     }
     return totalSales;
   },
-  getMorePages: function(url, totalSales){
+  getMorePages: function(url, totalSales, dataIn, catagorySalesIn){
     //parse url from returned url string;
+    var dataArray = dataIn;
     var salesTotal = totalSales;
+    var catagorySales = catagorySalesIn
+
     for ( var i = 0; i < url.length; i++){
       if (url.charAt(i) == '>'){
         url = url.substr(1, (i - 1));
@@ -70,17 +77,20 @@ module.exports = {
                            'cache-control': 'no-cache',
                             'content-type': 'application/json',
                                     accept: 'application/json',
+                                     limit: '200',
                              authorization: 'Bearer ' + process.env.STELLAS_ACCESS_TOKEN
                          }
                     }
       request(options, function(err, res, data){
         data = JSON.parse(data);
         salesTotal += module.exports.getTotalSales(data);
-
+        catagorySales.catagories = module.exports.getCatagorySales(data);
+        console.log(catagorySales.catagories);
+        console.log(salesTotal);
         if (err) reject(err)
 
         if (res.headers.link) {
-          salesTotal = module.exports.getMorePages(res.headers.link, salesTotal);
+          salesTotal = module.exports.getMorePages(res.headers.link, salesTotal, data, catagorySales);
           fulfill(salesTotal);
         }
         if (!res.headers.link) {
@@ -88,9 +98,29 @@ module.exports = {
         }
       });
     });
+  },
+  getCatagorySales: function(data){
+    var catagorySales = {};
+    catagorySales.catagories = {};
+
+    for (var i in data){
+      for (var e in data[i].itemizations)
+        var catagoryName = data[i].itemizations[e].item_detail.category_name;
+        if(!catagoryName) {
+          catagorySales.catagories = { 'un-catagorized': 1 };
+        } else {
+            catagorySales.catagories[catagoryName] = 1;
+          // } else {
+          //   catagorySales.catagories = data[i].itemizations[e].item_detail.category_name;
+          // }
+        }
+    }
+    console.log(catagorySales);
+    return catagorySales;
   }
 }
 
+//TODO: consolidate all sales data into single object
 
 // paths to values +==}========>
 
